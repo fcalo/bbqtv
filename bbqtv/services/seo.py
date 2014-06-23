f8ba2b7632453992df52727a5d4e1a9b5ef260c5
fer <fer@fer-945GCM-S2L.(none)>
2014-06-23 14:32:16 +0200

    seo keywords

---
+++ bbqtv/services/seo.py
@@ -0,0 +1,15 @@
+
+
+def smart_truncate(content, length=60, suffix=''):
+	if len(content) <= length:
+		return content
+	else:
+		return ' '.join(content[:length+1].split(' ')[0:-1]) + suffix
+
+class SEO(object):
+	def __init__(self):
+		pass
+		
+	def get_keywords(self, data):
+		
+		return smart_truncate(", ".join([d['name'] if 'name' in d else d['title'] for d in data]))
