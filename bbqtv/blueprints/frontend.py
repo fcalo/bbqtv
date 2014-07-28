df8372b09063eb28d17c750a20ceda60c6b96026
fer <fer@fer-945GCM-S2L>
2014-07-28 12:50:00 +0200

    rehome

--- bbqtv/blueprints/frontend.py
+++ bbqtv/blueprints/frontend.py
@@ -4,38 +4,83 @@ from flask import Blueprint, render_template, g, jsonify, request
 from flask.ext.sendmail import Message
 from bbqtv.services import bbqtv_db, mail
 from bbqtv.services import seo
-from datetime import datetime
+from datetime import datetime, timedelta
 import time
 
 frontend = Blueprint('frontend', __name__)
 
 @frontend.route('/')
 def index():
-	g.title = u"Programación TV hoy"
-	channels = bbqtv_db.get_channels()
-	keywords = seo.get_keywords(channels.clone())
-	g.keywords = keywords.lower()
-	g.description = u"Toda la programación de la TV. ¿Que quieres ver hoy? La programación de %s y más" % keywords
-	print channels
-	return render_template('index.html', channels = channels, grid = bbqtv_db.get_grid())
+    g.title = u"Programación TV hoy"
+    channels = bbqtv_db.get_channels()
+    keywords = seo.get_keywords(channels.clone())
+    g.keywords = keywords.lower()
+    g.description = u"Toda la programación de la TV. ¿Que quieres ver hoy? La programación de %s y más" % keywords
+    return render_template('index.html', channels = channels, now = bbqtv_db.get_now())
 
 
 @frontend.route('/programacion/<channel_id>')
 @frontend.route('/programacion/<channel_id>/<date>')
 def programacion( channel_id, date = None):
-	
-	channel = bbqtv_db.get_channel_day(channel_id, datetime.strptime(date, "%Y%m%d") if date else datetime.now())
-	
-	keywords = seo.get_keywords(channel['programmes'])
-	g.keywords = keywords.lower()
-	g.description = u"Toda la programación de %s. Todos los programas %s y más" % (channel['name'], keywords)
-	
-	g.title = u"Programación %s %s" % (channel['name'], "Hoy" if not date else channel['date'].strftime('%d/%m/%Y'))
-	
-	return render_template('channel.html', \
-	  channels = bbqtv_db.get_channels() , \
-	  channel = channel, \
-	  now = not date)
+    
+    d = datetime.strptime(date, "%Y%m%d") if date else datetime.now()
+    channel = bbqtv_db.get_channel_day(channel_id, d)
+    
+    
+    keywords = seo.get_keywords(channel['programmes']) if 'programmes' in channel else ""
+    
+    g.keywords = keywords.lower()
+    g.description = u"Toda la programación de %s. Todos los programas %s y más" % (channel['name'], keywords)
+    
+    
+    months = {
+        1: "Enero",
+        2: "Febrero",
+        3: "Marzo",
+        4: "Abril",
+        5: "Mayo",
+        6: "Junio",
+        7: "Julio",
+        8: "Agosto",
+        9: "Septiembre",
+        10: "Octubre",
+        11: "Noviembre",
+        12: "Diciembre",
+    }
+    
+    weekday={
+        0 : u"Lunes", 
+        1 : u"Martes", 
+        2 : u"Miércoles",
+        3 : u"Jueves", 
+        4 : u"Viernes", 
+        5 : u"Sábado",
+        6 : u"Domingo"
+    }
+    
+    data_next = {}
+    d_next = d + timedelta(days = 1)
+    print d_next
+    data_next['link'] = "/programacion/%s/%s" % (channel_id, d_next.strftime("%Y%m%d"))
+    data_next['day_of_week'] = weekday[d_next.weekday()]
+    data_next['day'] = d_next.day
+    data_next['month'] = months[d_next.month]
+    data_prev = {}
+    d_prev = d - timedelta(days = 1)
+    data_prev['link'] = "/programacion/%s/%s" % (channel_id, d_prev.strftime("%Y%m%d"))
+    data_prev['day_of_week'] = weekday[d_prev.weekday()]
+    data_prev['day'] = d_prev.day
+    data_prev['month'] = months[d_prev.month]
+    
+    g.title = u"Programación %s %s" % (channel['name'], "Hoy" if not date else d.strftime('%d/%m/%Y'))
+    
+    return render_template('channel.html', \
+      channels = bbqtv_db.get_channels() , \
+      channel = channel, \
+      data_next = data_next, \
+      data_prev = data_prev, \
+      now = not d,
+      date = d)
       
 @frontend.route('/contact', methods=['GET', 'POST'])
 def contact():
--- bbqtv/services/db.py
+++ bbqtv/services/db.py
@@ -2,36 +2,56 @@ from pymongo import MongoClient
 from datetime import datetime
 
 class DB(object):
-	def __init__(self):
-		pass
-		
-	def init_app(self, app):
-		self.db = MongoClient(host = app.config['DB_HOST'], 
-		  port = app.config['DB_PORT'])[app.config['DB_NAME']]
-		
-	def get_channels(self, limit = None):
-		if limit:
-			return self.db.channels.find().limit(limit).sort("order", 1)
-		else:
-			return self.db.channels.find().sort("order", 1)
+    def __init__(self):
+        pass
+        
+    def init_app(self, app):
+        self.db = MongoClient(host = app.config['DB_HOST'], 
+          port = app.config['DB_PORT'])[app.config['DB_NAME']]
+        
+    def get_channels(self, limit = None):
+        if limit:
+            return self.db.channels.find().limit(limit).sort("order", 1)
+        else:
+            return self.db.channels.find().sort("order", 1)
 
 
-	def get_grid(self):
-		grid = {}
-		for c in self.get_channels(limit = 7):
-			grid[c['name']] = self.get_channel_day(c['_id'], datetime.now())
-		
-		return grid
-			
+    def get_grid(self):
+        grid = {}
+        for c in self.get_channels(limit = 7):
+            grid[c['name']] = self.get_channel_day(c['_id'], datetime.now())
+        
+        return grid
+        
+    def get_now(self):
+        grid = {}
+        for c in self.get_channels(limit = 7):
+            now = datetime.now()
+            day = self.get_channel_day(c['_id'], now)
+            hour, minute = (now.hour, now.minute)
+            
+            last_programme = None
+            grid[c['name']] = {}
+            for programme in day['programmes']:
+                
+                hour_pro, minute_pro = [int(a) for a in programme['time'].split(":")]
+                
+                if hour < hour_pro or ( hour == hour_pro and minute <= minute_pro):
+                    grid[c['name']]['now'] = last_programme
+                    grid[c['name']]['next'] = programme
+                    break
+                last_programme = programme
+        return grid
+            
 
 
-	def get_channel_day(self, collection_name, date):
-		channel_data = self.db[collection_name].find_one({"date" : datetime(date.year, date.month, date.day) })
-		if not channel_data:
-			channel_data = {}
-		
-		channel = self.db.channels.find_one({"_id" : collection_name})
-		if channel:
-			channel_data['name'] = channel['name']
-		
-		return channel_data
+    def get_channel_day(self, collection_name, date):
+        channel_data = self.db[collection_name].find_one({"date" : datetime(date.year, date.month, date.day) })
+        if not channel_data:
+            channel_data = {}
+        
+        channel = self.db.channels.find_one({"_id" : collection_name})
+        if channel:
+            channel_data['name'] = channel['name']
+        
+        return channel_data
